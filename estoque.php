236091c9e37ea19aa8b2c1ad53f8b6a622d041b7
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2019-08-30 20:19:32 -0300

    Add files via upload

--- estoque.php
+++ estoque.php
@@ -15,8 +15,6 @@
 		
 		<button type="button" class="btn btn-primary btn-md ml-5" data-toggle="modal" data-target="#modal1">Cadastro de Produtos</button>
         <button type="button" class="btn btn-dark">Imprimir</button>
-
-
         <!--Modal-->
 			<div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
 				<div class="modal-dialog modal-dialog-centered" role="document">
@@ -27,7 +25,6 @@
 								<span aria-hidden="true">&times;</span>
 							</button>
 						</div>
-
 			      		<div class="modal-body">
 			        		<h5>Descrição do Material</h5>
 							<form class = "form-group mt-2" action="agendarCliente.php" method="post">
@@ -67,47 +64,65 @@
 					</div>
 				</div>
 			</div>
+			
+<div class="container">
+ <div class= "row">
+
+<div class = "col-4">
+  <ul class="list-group"><t3>Material</t3>
+  <li class="list-group-item active"></li>
+  <li class="list-group-item">Gaze</li>
+  <li class="list-group-item">Touca</li>
+  <li class="list-group-item">Mascara</li>
+  <li class="list-group-item">Algodão</li> 
+  <li class="list-group-item">Álcool 70</li> 
+  <li class="list-group-item">Água Destilada</li>
+  <li class="list-group-item">Pró-pe</li>
+  <li class="list-group-item">Babador</li>
+  <li class="list-group-item">Agulha</li>
+  <li class="list-group-item">Fio de Sultura</li>
+  <li class="list-group-item">Luva</li>
+  <li class="list-group-item">>Luva P</li>
+  <li class="list-group-item">>Luva M</li>
+  <li class="list-group-item">>Luva G</li>
+  </ul>
+</div>
 		
-		<table class="table w-75">
+		<table class="table w-100 col-8 mt-4">
   <thead class="thead-dark">
     <tr>
       <th scope="col">Nome</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Codigo</th>
+      <th scope="col">Categoria</th>
+      <th scope="col">Unidade</th>
+      <th scope="col">Custo (por unidade)</th>
+      <th scope="col">Validade</th>
+      <th scope="col">Disponivel</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">Codigo</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
     </tr>
     <tr>
-      <th scope="row">Categoria</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
     </tr>
     <tr>
-      <th scope="row">Un</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
     </tr>
   </tbody>
 </table>		
-        
-        <ul class="list-group"><t3>Material</t3>
-  <li class="list-group-item active"></li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
-</ul>
-        
-        
+</div>       
+</div>        
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
