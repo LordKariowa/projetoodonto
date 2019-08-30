3fd6e85d24365349dbdfcea62812ae89969e0dde
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2019-08-29 21:50:44 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,9 +1,3 @@
 <?php
-$servername = "localhost"; //servidor (local)
-$database = "odontologico"; //nome do banco, pode alterar
-$username = "root"; // usuário padrão
-$password = ""; // senha de conexão do banco de dados.
-
-// Create connection
-$conexao = mysqli_connect($servername, $username, $password, $database);
+	$con = mysqli_connect("localhost", "root", "", "teste");
 ?>
