86b6633448dc30047857757c84cb6769abd4b6ae
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2019-08-30 21:45:32 -0300

    Add files via upload

---
+++ ficha.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Odonto-Ficha</title>
+    	<meta charset="utf-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="styleHeader.css">
+		<link rel="stylesheet" type="text/css" href="styleCadastro.css">
+</head>
+<body>
+    	      		<button class="btn btn-info btn-md mr-3" type="submit">Pesquisar Cadastro</button>
+    	      		<button type="button" class="btn btn-primary">Novo Paciente</button>
+    	      		<button type="button" class="btn btn-dark">Imprimir</button>
+    	      		
+    	      		
+<table class="table table-hover table-blue">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+</body>
+</html>
