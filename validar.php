1c8b6c96afc7d4e49a4220f4b8fed71b8e54e45f
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2019-12-05 21:05:45 -0300

    Add files via upload

--- validar.php
+++ validar.php
@@ -1,8 +1,8 @@
-<?php
-    session_start();
-    if(!isset($_SESSION["login"])){
-        session_destroy();
-        $msg = "Usuário sem acesso!";
-        header("Location:login.php?erro=".$msg);
-    }
-?>
+<!-- <?php
+ //   session_start();
+//    if(!isset($_SESSION["login"])){
+ //       session_destroy();
+   //     $msg = "Usuário sem acesso!";
+   //     header("Location:login.php?erro=".$msg);
+   // }
+?> -->
